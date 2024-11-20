cdbb1114c69c4b380294d368a094f1bead30138f
Alexandru Edves <95491924+AlexE10@users.noreply.github.com>
2024-11-20 10:59:17 +0200

    Created UI for login and signup pages.

--- TalentBridge/app/build.gradle.kts
+++ TalentBridge/app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
 
     implementation("androidx.recyclerview:recyclerview:1.3.1")
 
+    implementation("com.google.android.material:material:1.9.0")
+
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
